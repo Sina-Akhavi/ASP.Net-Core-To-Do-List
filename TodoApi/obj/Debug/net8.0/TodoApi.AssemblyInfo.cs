ef134c7dadab2e2d026375a3a6d4fd972addc6d4
Sina Akhavi <sinaakhavispecial1984@gmail.com>
2026-09-02 10:22:52 +0330

    added dlls

--- TodoApi/obj/Debug/net8.0/TodoApi.AssemblyInfo.cs
+++ TodoApi/obj/Debug/net8.0/TodoApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+29b0d9f70dac30e7e976e43c508824095aee2021")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+17bbd6872721b00fd01529e5d86ecd8de450936b")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
